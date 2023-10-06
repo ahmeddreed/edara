37954f76ccf38e664f03c931e2db388b81501b8e
ahmeddreed <iraq07715763814@gmail.com>
2023-10-06 22:40:30 +0300

    test 3

--- resources/views/livewire/auth/auth.blade.php
+++ resources/views/livewire/auth/auth.blade.php
@@ -3,9 +3,9 @@
         <div class="col-lg-10 mx-auto text-center my-5">
             <div class="card shadow color">
                 <div class="card-body d-flex justify-content-around">
-                    <button wire:click='changeTheAction("login")' class="btn btn-light px-3 bg fw-bold">تسجيل الدخول</button>
-                    <button wire:click='changeTheAction("customer login")' class="btn color px-3 fw-bold">تسجيل الدخول عميل</button>
-                    <button wire:click='changeTheAction("customer register")' class="btn color px-3 fw-bold">انشاء حساب عميل</button>
+                    <button wire:click='changeTheAction("login")' class="btn btn-light px-3 {{ $action == "login" ? "bg" : "color"  }}  fw-bold">تسجيل الدخول</button>
+                    <button wire:click='changeTheAction("customer login")' class="btn btn-light  {{ $action == "customer login" ? "bg" : "color"  }} px-3 fw-bold">تسجيل الدخول عميل</button>
+                    <button wire:click='changeTheAction("customer register")' class="btn btn-light {{ $action == "customer register" ? "bg" : "color"  }}  px-3 fw-bold">انشاء حساب عميل</button>
                 </div>
             </div>
         </div>
